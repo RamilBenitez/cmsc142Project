725d28c9d8d145c02f36ad3ab95a2925839be258
RamBenitez2721 <ribenitez>
2025-05-01 13:36:29 +0800

    Add function for minimum vertex cover andd solving min vertex cover with tree given

--- vertexCoverDP.c
+++ vertexCoverDP.c
@@ -19,3 +19,55 @@ Node* newNode(int data){
     node -> left = node->right = NULL; 
     return node;
 }
+
+// helper to calculate vertex cover size
+void vertexCover(Node* root, int* include, int* exclude){
+    if(!root){
+        *include =0;
+        *exclude =0;
+        return;
+    }
+
+    //stores resuts from subtrees
+    int left_include, left_exclude;
+    int right_include, right_exclude;
+
+    //calculate for right and lefr subtress recursively
+    vertexCover(root->left, &left_include, &left_exclude);
+    vertexCover(root->right, &right_include, &right_exclude);
+
+    //including the current node in vertex cover
+    *include = 1 + (left_include < left_exclude ? left_include : left_exclude)// min of left
+    + (right_include <right_exclude ? right_include : right_exclude); // min of right
+
+    //excluding thee current node in vertex cover
+    *exclude = left_include  + right_include;
+    }
+
+    //main function to compute min vertex cover
+int minVertexCover(Node* root) {
+    int include, exclude;
+
+    vertexCover(root, &include, &exclude);
+
+     //retrn the smaller
+    return(include < exclude) ? include:exclude;
+    }
+
+//main program
+
+int main(){
+
+    //tree
+    Node* root = newNode(10);
+    root->left = newNode(20);
+    root->right = newNode(30);
+    root->left->left =newNode(40);
+    root->left->right=newNode(50);
+    root->right->right=newNode(60);
+
+    printf("Minimum Vertex Cover Size: %d\n", minVertexCover(root));
+
+    return 0;
+    
+}
